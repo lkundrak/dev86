ae7c628a6664e69bbd276665442107858740558c
mfld-fr <mfld.fr+github@gmail.com>
2017-01-18 23:40:58 +0100

    [mon86] Makefile for host part + more testing

--- mon86/build.sh
+++ mon86/build.sh
@@ -1,24 +1,15 @@
-#/bin/sh
-
-# Build stub for host
-
-gcc -g -D HOST_STUB -o pt1-stub pt1-target-main.c
-gcc -g -D HOST_STUB -o mon86-stub mon86-common.c mon-target.c
+#!/bin/bash
 
 # Build binaries for target
 
-bcc -ansi -0 -c pt1-target-main.c
-ld86 -0 -d -M -o pt1-target pt1-target-main.o > pt1-target.map
+# First stage monitor
 
-bcc -ansi -0 -c -o mon86-common.o mon86-common.c
-bcc -ansi -0 -c -o mon-target.o mon-target.c
+bcc -ansi -0 -c pt1-target.c
+ld86 -0 -d -M -o pt1.bin pt1-target.o > pt1.map
+
+# Second stage monitor
+
+bcc -ansi -0 -c -o mon86-common-target.o mon86-common.c
+bcc -ansi -0 -c -o mon86-target.o mon86-target.c
 as86 -0 -o mon86-low.o mon86-low.s
-ld86 -0 -d -M -o mon86.bin mon86-low.o mon86-common.o mon-target.o > mon86.map
-
-# Build tools for host
-
-gcc -g -o pt1-read pt1-read.c
-gcc -g -o pt1-write pt1-write.c
-gcc -g -o pt1-exec pt1-exec.c
-
-gcc -g -o mon86 mon86-common.c mon-host.c
+ld86 -0 -d -M -o mon86.bin mon86-low.o mon86-common-target.o mon86-target.o > mon86.map
